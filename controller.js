0140fba650a0cfc1240334a80a604947a6c2a8e1
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2016-11-15 13:00:23 -0700

    finished instructions

--- controller.js
+++ controller.js
@@ -1,5 +1,17 @@
-angular.module('userProfiles').controller('MainController', function($scope){
-    
+angular.module('userProfiles').controller('MainController', function($scope, mainService){
+
     $scope.thisAppIsBroken = "This angular app is working";
-   
+
+    $scope.getUsers = function() {
+      $scope.users = mainService.getUsers();
+    }
+
+    $scope.getUsers();
+
+    // $scope.toggleFavorite = function() {
+    //   return mainService.toggleFavorite();
+    // }  this is not quite right
+
+    $scope.toggleFavorite = mainService.toggleFavorite
+
 })
